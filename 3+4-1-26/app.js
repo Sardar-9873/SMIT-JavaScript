55e335f7e6a3ef3dc0e6522111343b57a112b132
Sardar-9873 <zafarsabashehzad@gmail.com>
2026-01-03 22:19:51 +0500

    updated inde.html 3/1/26

---
+++ 3+4-1-26/app.js
@@ -0,0 +1,16 @@
+// alert("My name is M.Sahal Shehzad");
+// alert("I am a web developer");
+// alert("I also study in school");
+// alert("I am in class 8");
+// alert("I am 13 years old");
+// alert("I want to be a military captain doctor in future");
+// alert("These all alerts are for practice");
+
+// var myName = "Sahal"
+
+// let my_age1 = 13 + 2 - 1 / (5 * 5) * 25
+
+// console.log(my_age1)
+
+// alert("Assalamualaikum");
+
