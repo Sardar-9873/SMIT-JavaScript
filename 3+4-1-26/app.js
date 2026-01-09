b25dd13faae914944814396f10bec8367b10addf
Sardar-9873 <zafarsabashehzad@gmail.com>
2026-01-09 18:43:42 +0500

    Update app.js

--- 3+4-1-26/app.js
+++ 3+4-1-26/app.js
@@ -1,16 +1,16 @@
-// alert("My name is M.Sahal Shehzad");
-// alert("I am a web developer");
-// alert("I also study in school");
-// alert("I am in class 8");
-// alert("I am 13 years old");
-// alert("I want to be a military captain doctor in future");
-// alert("These all alerts are for practice");
+alert("Assalamualaikum");
+alert("My name is M.Sahal Shehzad");
+alert("I am a web developer");
+alert("I also study in school");
+alert("I am in class 8");
+alert("I am 13 years old");
+alert("I want to be a military captain doctor in future");
+alert("These all alerts are for practice");
 
-// var myName = "Sahal"
+var myName = "Sahal"
 
-// let my_age1 = 13 + 2 - 1 / (5 * 5) * 25
+var myAge1 = 13 + 2 - 1 / (5 * 5) * 25
 
-// console.log(my_age1)
+console.log(myAge1)
 
-// alert("Assalamualaikum");
 
