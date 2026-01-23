9ef67ecca269de24950fddf69fc4f6ecf880089d
Sardar-9873 <zafarsabashehzad@gmail.com>
2026-01-23 19:17:27 +0500

    updated app.js by if statements

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-var userAge = prompt(`Enter Your Year of Birth!!!`);
-var updatedUserAge = 2026 - userAge;
-alert("Your Age is "+updatedUserAge);
+// var userAge = prompt(`Enter Your Year of Birth!!!`);
+// var updatedUserAge = 2026 - userAge;
+// alert("Your Age is " + updatedUserAge);
+
+// const myName = prompt("Enter your Name");
+
+// const myFatherName = "Shehzad";
+
+// if (myName === "Mustafa") alert("Hello")
+// else if (myName === "Kamran") alert("Hi")
+// else if (myName === "Arsalan") alert("Assalamualaikum")
+// else if (myName === "Azan") alert("Bye")
+// else {
+//     const myState = prompt("How are you?")
+// }
+
+
+// const isPowerFull = true;
+// const isIntelligent = true;
+// const isYoung = true;
+// const isHealthy = true;
+// const isClever = true;
+
+
+// if (isPowerFull && isIntelligent && isClever && isYoung && isHealthy) {
+//     alert("You are selected in army")
+// }
+
+// alert("Battle Started")
+
+// if (isPowerFull || isIntelligent || isClever || isYoung || isHealthy) {
+//     alert("You are selected in army")
+// }
+
+
+
